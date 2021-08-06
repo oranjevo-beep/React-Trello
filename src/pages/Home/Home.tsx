2b1a6f10553432ef12971a3766c9c86b4f7e738a
Denys Yelienchuk <oranjevo@gmail.com>
2021-08-06 11:03:09 +0300

    changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,15 +27,19 @@ const Home = () => {
   );
 };
 const BoardLink = (props: IBoardLink) => {
-  const colors = ['f94144', 'f3722c', '43aa8b', '90be6d', 'f8961e', '577590'];
-  let randomColor = (arr: string[]) => {
-    let random = Math.floor(Math.random() * arr.length);
-    return random;
-  };
-  let randomC = `#${colors[randomColor(colors)]}`;
+  // const colors = ['f94144', 'f3722c', '43aa8b', '90be6d', 'f8961e', '577590'];
+  // let randomColor = (arr: string[]) => {
+  //   let random = Math.floor(Math.random() * arr.length);
+  //   return random;
+  // };
+  // let randomC = `#${colors[randomColor(colors)]}`;
+  function generateRandomColor() {
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  }
 
   return (
-    <div className="board-link" style={{ background: randomC }}>
+    <div className="board-link" style={{ background: generateRandomColor() }}>
       <h4>{props.title}</h4>
     </div>
   );
